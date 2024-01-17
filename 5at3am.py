b19b31088a9a97996736c523b6512937364ccee2
NufalXBaalash <156456394+NufalXBaalash@users.noreply.github.com>
2024-01-17 06:06:45 +0200

    5at3am.py

--- 5at3am.py
+++ 5at3am.py
@@ -1,5 +1,12 @@
 import customtkinter as ctk
 from tkinter import *
+import tkinter as tk
+from openpyxl import load_workbook
+
+wb = load_workbook("E:\College\C++ Codes\Python Codes\Khat3am_Project\Khat3am.xlsx", data_only=True)
+ws = wb.active
+
+
 
 calculation = 0
 foul=0
@@ -7,30 +14,34 @@ foul=0
 
 
 
+
 def next_person():
     text_result.insert("end" , "\n")
 
+def open_new_window():
+    new_window = Toplevel(root)
+    new_window.title("Final Order")
+    new_window.geometry("500x420")
+    new_window.grid_columnconfigure((4) , weight=2)
+    new_window.grid_rowconfigure((4) , weight=2)
+    Final_order = tk.Text(new_window , height=4 , width=35 , font=("Arial", 18))
+    Final_order.grid(row=1 , column=0 , sticky="n" , padx=20 , pady=10 , columnspan=5)
+    for row in range(2, 48):  # Adjust the range to match your requirements
+        column_J = ws['J' + str(row)].value
+        value_A = ws['A' + str(row)].value
 
+        if column_J is not None and value_A is not None and value_A != 0:
+            Final_order.insert( 1.0  , f" Name : {column_J} , Pay : {value_A}\n")
+    All_types = tk.Text(new_window, height=3 , width=40 , font=("Arial" , 20))
+    All_types.grid(row=0 , column=0 , padx=20 , pady=10 , columnspan=5)
+    Total = tk.Text(new_window, height=3 , width=40 , font=("Arial" , 20))
+    Total.grid(row=2 , column=0 , padx=20 , pady=10 , columnspan=5)
 
+names = []
+for i in range (2 , 48):
+    x = ws['J' + str(i)].value
+    names.append(x)
 
-def add_to_calculation(symbol,type_name):
-    
-    global calculation
-    calculation += int(symbol)
-    text_result.delete(1.0)
-    text_result.insert(1.0 , type_name)
-    text_result.insert(1.0 , calculation)
-
-
-def evaluate_calculation():
-    global calculation
-    try:
-        calculation = str(eval(calculation))
-        text_result.delete(1.0 , "end")
-        text_result.insert(1.0 , calculation)
-    except:
-        clear_field()
-        text_result.insert(1.0 , "Error")
 
 def clear_field():
     global calculation
@@ -44,23 +55,28 @@ def fplus_food(var):
     text_result.insert(1.0 , var)
 
 
+def search_items():
+    search_value = variable.get().lower()
+    if not search_value.strip():
+        combo_names['values'] = item_names
+    else:
+        value_to_display = [str(value) for value in item_names if search_value in str(value).lower()]
+        combo_names['values'] = value_to_display
 
 
 root = ctk.CTk()
 root.geometry("817x636")
 root.title("ختعم")
-root.resizable(False, False)
+#root.resizable(False, False)
 root.grid_columnconfigure((8) , weight=2)
 root.grid_rowconfigure((4), weight=2)
 
 
+
+
+
 foul_name = "  فول   "
 
-
-
-"""w = ctk.CTkScrollbar(root , height=50 , width=50)
-w.grid(row=8 , column=8 , padx=10 , pady=10 ,sticky="ne" , columnspan=10 )
-"""
 
 
 scrollbar = ctk.CTkScrollbar(root , orientation="vertical")
@@ -73,12 +89,12 @@ frame2.grid_columnconfigure((5) , weight=2)
 frame2.grid_rowconfigure((8) , weight=2)
 frame2.grid(row=0 , column=0 , padx=20 , pady=20)
 
-text_result = ctk.CTkTextbox(frame2 , height=455 , width= 233, font=("Arial", 20))
-text_result.grid(row=0 , column=0 , padx=20 , pady=10, columnspan=10 , sticky="nw")
-text_Tperson = ctk.CTkTextbox(frame2 , height=455 , width= 70, font=("Arial", 20))
-text_Tperson.grid(row=0 , column=3 , padx=20 , pady=10, columnspan=10 , sticky="ne")
+text_result = ctk.CTkTextbox(frame2 , height=455 , width= 150, font=("Arial", 20))
+text_result.grid(row=2 , column=0 , padx=20 , pady=10, columnspan=10 , sticky="nw")
+text_Tperson = ctk.CTkTextbox(frame2 , height=455 , width= 150, font=("Arial", 20))
+text_Tperson.grid(row=2 , column=3 , padx=20 , pady=10, columnspan=10 , sticky="ne")
 text_total = ctk.CTkTextbox(frame2 , height=68 , width= 290, font=("Arial", 20))
-text_total.grid(row=2 , column=0 , padx=20 , pady=10, columnspan=10)
+text_total.grid(row=0 , column=0 , padx=20 , pady=10, columnspan=10)
 
 
 
@@ -93,28 +109,52 @@ Fbtn2.grid(row=0 , column=1, padx=5, pady=5)
 frame.grid(row=0 , column=4 , padx=20 , pady=20, sticky="ne", columnspan=10)
 
 
-frame3 = ctk.CTkFrame(root, height=244 , width=400)
+frame3 = ctk.CTkFrame(root, height=244 , width=460)
 frame3.grid_columnconfigure((4), weight=2)
 frame3.grid_rowconfigure((4), weight=2)
 frame3.grid(row=0 , column=4 , padx=20, pady=20 , sticky="se" , columnspan=10)
 
-btn_clear = ctk.CTkButton(frame3, text="Clear",command=lambda: clear_field(), height=80 , width=120)
+btn_clear = ctk.CTkButton(frame3, text="Clear",command=lambda: clear_field(), height=80 , width=80)
 btn_clear.grid(row=0 , column=1 , padx=20 , pady=20)
 
-btn_next = ctk.CTkButton(frame3, height=80 , width=120 , text="Next Person" , command=lambda: next_person())
+btn_next = ctk.CTkButton(frame3, height=80 , width=80 , text="Next Person" , command=lambda: next_person())
 btn_next.grid(row=0 , column=0 , padx=20 , pady=20 , sticky="w")
 
+btn_order = ctk.CTkButton(frame3, height=80, width=80 , command=lambda: open_new_window(), text="Order"  )
+btn_order.grid(row=0 , column=2 , padx=20 , pady=20)
 
-combo=ctk.CTkComboBox(root , height=50 , width=250 , values=Names , font=("Arial",20))
+"""combo=ctk.CTkComboBox(root , height=50 , width=250 , values=Names , font=("Arial",20))
 combo.grid(row=0 , column=10 , padx=10 , pady=10 , sticky="e")
-
-plus_food = ctk.CTkButton(root,height=60 , width=60 ,text="+" , command=lambda:fplus_food("+"))
+"""
+"""plus_food = ctk.CTkButton(root,height=60 , width=60 ,text="+" , command=lambda:fplus_food("+"))
 plus_food.grid(row=0 , column=4 , padx=20 , pady=20 , sticky="e" , columnspan=2)
+"""
+frame4 = ctk.CTkFrame(root , width=402 , height=120 )
+frame4.grid_columnconfigure((4) , weight=2)
+frame4.grid_rowconfigure((4) , weight=2)
+
+frame4.grid(row=0 , column=8 , padx=20 , pady=20 , sticky="e")
+
+add_btn = ctk.CTkButton(frame4 , height=50 , width=50 , text=" + " ,font=("Arial" , 26) , corner_radius=90)
+add_btn.grid(row=2 , column=1 , padx=10 , pady=20 , columnspan=1)
+
+minus_btn = ctk.CTkButton(frame4 , height=50 , width=50 , text=" - "  , font=("Arial" , 28) , corner_radius=90)
+minus_btn.grid(row=2 , column=0 , padx=20 , pady=20 , columnspan=1)
+
+combo_names = ctk.CTkComboBox(frame4 , height=20 , width=300 , font=("Arial" , 16) , values=names , )
+combo_names.grid(row=0 , column=0 , padx=20 , pady=0 , sticky="e" , columnspan=5)
 
 
+item_names = names
+variable = StringVar()
+combo_names['values'] = item_names
 
+variable = StringVar()
+entry1 = ctk.CTkEntry(frame4, textvariable=variable , height=20 , width=250)
+entry1.grid(row=1 , column=0 , padx=20 , pady=0 , columnspan=5 , sticky="e")
 
-
+Search_btn = ctk.CTkButton(frame4, text="Search", command=lambda:search_items , height=50 , width=100 , corner_radius=20)
+Search_btn.grid(row=2 , column = 3 , padx=20 , pady=5)
 
 
 
