ba05e0545338d2cb4ef6057155527e729648f9b1
NufalXBaalash <156456394+NufalXBaalash@users.noreply.github.com>
2024-01-19 19:40:33 +0200

    Update 5at3am.py

--- 5at3am.py
+++ 5at3am.py
@@ -4,6 +4,7 @@ from tkinter import *
 import tkinter as tk
 from openpyxl import load_workbook
 
+
 wb = load_workbook("E:\College\C++ Codes\Python Codes\Khat3am_Project\\asdasdasdas.xlsx" , data_only=True)
 ws = wb.active
 df = pd.read_excel("E:\College\C++ Codes\Python Codes\Khat3am_Project\\asdasdasdas.xlsx")
@@ -11,10 +12,13 @@ df = pd.read_excel("E:\College\C++ Codes\Python Codes\Khat3am_Project\\asdasdasd
 calculation = 0
 foul = 0
 
-    
+  
+
 
 def next_person():
-    text_result.insert("end", "\n")
+    text_result.delete(1.0, "end")
+    text_Tperson.delete(1.0, "end")
+    text_total.delete(1.0 , "end")
 
 
 def open_new_window():
@@ -23,7 +27,7 @@ def open_new_window():
     new_window.geometry("500x420")
     new_window.grid_columnconfigure((4), weight=2)
     new_window.grid_rowconfigure((4), weight=2)
-    total_cost = 0
+    total_cost = 10
     foul_total = 0
     tamea_total = 0
     patats_total = 0
@@ -77,12 +81,15 @@ def open_new_window():
 
 
 names = []
-for i in range(2, 48):
+for i in range(2, 49):
     x = ws["J" + str(i)].value
     names.append(x)
 
 def Name_select():
     global Var_row
+    text_result.delete(1.0, "end")
+    text_Tperson.delete(1.0, "end")
+    text_total.delete(1.0 , "end")
     target_name = combo_names.get()
     text_total.delete(1.0 , "end")
     text_total.insert(1.0 , target_name)
@@ -94,16 +101,10 @@ def Name_select():
 
 
 def clear_field():
-    for row in range(2 , 48):
+    for row in range(2 , 49):
         for column in range(66 , 74):
             ws[str(chr(column)) + str(row)].value = 0
     wb.save("E:\College\C++ Codes\Python Codes\Khat3am_Project\\asdasdasdas.xlsx")
-
-
-def fplus_food(var):
-    global calculation
-    calculation = 0
-    text_result.insert(1.0, var)
 
 
 def search_items():
@@ -117,10 +118,13 @@ def search_items():
         combo_names["values"] = value_to_display
 
 
+
+ctk.set_default_color_theme("E:\College\C++ Codes\Python Codes\Khat3am_Project\Color_themes\Theme 5.json")
 root = ctk.CTk()
-root.geometry("817x636")
-root.title("ختعم")
-# root.resizable(False, False)
+root.geometry("800x600")
+root.title("5at3am")
+root.after(201, lambda :root.iconbitmap("E:\College\C++ Codes\Python Codes\Khat3am_Project\photo_2024-01-18_19-18-03.ico"))
+root.resizable(False, False)
 root.grid_columnconfigure((8), weight=2)
 root.grid_rowconfigure((4), weight=2)
 
@@ -128,11 +132,6 @@ root.grid_rowconfigure((4), weight=2)
 foul_name = "  فول   "
 
 
-scrollbar = ctk.CTkScrollbar(root, orientation="vertical")
-Names = ["Ahmed Baalash", "Yousef Fady", "Ahmed Foudah"]
-"""k = ctk.CTkOptionMenu(root , height= 50, width=120 , font=("Vivaldi", 24) , anchor="w" , values=Names )
-k.grid(row=0 , column=8 , padx=20 , pady=20, sticky="e", columnspan=10)
-"""
 frame2 = ctk.CTkFrame(root, width=340, height=566)
 frame2.grid_columnconfigure((5), weight=2)
 frame2.grid_rowconfigure((8), weight=2)
@@ -169,6 +168,7 @@ def plus():
     main +=1
     ws[column2 + str(Var_row)].value +=1
     text_Tperson.delete(1.0 , 2.0)
+    text_Tperson.insert(1.0, "\n")
     text_Tperson.insert(1.0 ,main)
 
 
@@ -178,9 +178,10 @@ def minus():
     int(main)
     main -=1
     ws[column2 + str(Var_row)].value -=1
-    text_Tperson.delete(1.0 , "end")
+    text_Tperson.delete(1.0 , 2.0)
+    text_Tperson.insert(1.0, "\n")
     text_Tperson.insert(1.0 , main)
-
+                        
 Fbtn1 = ctk.CTkButton(
     frame,
     text=foul_name,
@@ -276,17 +277,17 @@ frame3.grid(row=0, column=4, padx=20, pady=20, sticky="se", columnspan=10)
 btn_clear = ctk.CTkButton(
     frame3, text="Clear", command=lambda: clear_field(), height=80, width=80
 )
-btn_clear.grid(row=0, column=1, padx=20, pady=20)
+btn_clear.grid(row=1, column=1, padx=20, pady=20)
 
 btn_next = ctk.CTkButton(
     frame3, height=80, width=80, text="Next Person", command=lambda: next_person()
 )
-btn_next.grid(row=0, column=0, padx=20, pady=20, sticky="w")
+btn_next.grid(row=1, column=0, padx=20, pady=20, sticky="w")
 
 btn_order = ctk.CTkButton(
     frame3, height=80, width=80, command=lambda: open_new_window(), text="Order"
 )
-btn_order.grid(row=0, column=2, padx=20, pady=20)
+btn_order.grid(row=1, column=2, padx=20, pady=20)
 
 """combo=ctk.CTkComboBox(root , height=50 , width=250 , values=Names , font=("Arial",20))
 combo.grid(row=0 , column=10 , padx=10 , pady=10 , sticky="e")
@@ -294,21 +295,21 @@ combo.grid(row=0 , column=10 , padx=10 , pady=10 , sticky="e")
 """plus_food = ctk.CTkButton(root,height=60 , width=60 ,text="+" , command=lambda:fplus_food("+"))
 plus_food.grid(row=0 , column=4 , padx=20 , pady=20 , sticky="e" , columnspan=2)
 """
-frame4 = ctk.CTkFrame(root, width=402, height=120)
+frame4 = ctk.CTkFrame(root, width=500, height=120 , corner_radius=0)
 frame4.grid_columnconfigure((4), weight=2)
 frame4.grid_rowconfigure((4), weight=2)
 
-frame4.grid(row=0, column=8, padx=20, pady=20, sticky="e")
+frame4.grid(row=0, column=8, padx=20, pady=100, sticky="e")
 
 add_btn = ctk.CTkButton(
     frame4, height=50, width=50, text=" + ", font=("Arial", 26), corner_radius=90 , command=lambda: plus()
 )
-add_btn.grid(row=2, column=1, padx=10, pady=20, columnspan=1)
+add_btn.grid(row=2, column=1, padx=20, pady=30, columnspan=1)
 
 minus_btn = ctk.CTkButton(
     frame4, height=50, width=50, text=" - ", font=("Arial", 28), corner_radius=90 , command=lambda: minus()
 )
-minus_btn.grid(row=2, column=0, padx=20, pady=20, columnspan=1)
+minus_btn.grid(row=2, column=0, padx=20, pady=30, columnspan=1)
 
 combo_names = ctk.CTkComboBox(
     frame4,
@@ -319,7 +320,7 @@ combo_names = ctk.CTkComboBox(
 
 )
 
-combo_names.grid(row=0, column=0, padx=20, pady=0, sticky="e", columnspan=5)
+combo_names.grid(row=0, column=0, padx=20, pady=15, sticky="e", columnspan=5)
 
 
 item_names = names
@@ -338,7 +339,8 @@ Search_btn = ctk.CTkButton(
     width=100,
     corner_radius=20,
 )
-Search_btn.grid(row=2, column=3, padx=20, pady=5)
+Search_btn.grid(row=2, column=3, padx=30, pady=5)
 
-
+Makers = ctk.CTkLabel(frame3 , text="  Ahmed Baalash, Yousef Fady, Ahmed Fouda" , font=("Mistral" , 24) , text_color="#332941")
+Makers.grid(row=0 , column=0 , padx=5 , pady=10 , sticky='w' , columnspan=5)
 root.mainloop()
